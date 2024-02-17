38552ba360903363fd7e794eb070b30f92220fa8
Cpanelli <133393733+Cpanelli@users.noreply.github.com>
2024-02-16 23:27:32 -0500

    made changes to the files in the modesl folder

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection.js');
+const { Model, DataTypes } = require("sequelize");
+const dbConnection = require("../config/connection.js");
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+Category.initialize = () => {
+  Category.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      category_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize: dbConnection,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: "category",
+    }
+  );
+};
 
 module.exports = Category;
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,40 @@
-const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const dbConnection = require("../config/connection");
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
-);
+ProductTag.initialize = () => {
+  ProductTag.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "product",
+          key: "id",
+        },
+      },
+      tag_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "tag",
+          key: "id",
+        },
+      },
+    },
+    {
+      sequelize: dbConnection,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: "product_tag",
+    }
+  );
+};
 
 module.exports = ProductTag;
